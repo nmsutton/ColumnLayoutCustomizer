55286c8a7ab6f24f80af9ce3f91032f7d117c744
Nate Sutton <natemsutton@yahoo.com>
2013-07-22 19:35:44 -0700

    improved number series generation algorithm to position the numbers better

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -7,6 +7,9 @@
  * the generated numbers should be in.  Calculations are run based on those values to create a container
  * for the number series with the right dimensions.  Css styling allows the number to be displayed in the
  * intended format once the container with the customized dimensions is created.
+ *
+ * References:
+ * http://stackoverflow.com/questions/1829925/javascript-getelementbyid-not-working
  */
 
 ActivateCounterSwitch = false;
@@ -25,26 +28,64 @@ function IncreaseItemsList() {
 		var MinimumColumnHeightUserInput = $('input#MinimumColumnHeight').val();
 		var MaximumNumberOfColumnsUserInput = $('input#MaximumNumberOfColumns').val();
 		var HeightOfIndividualNumbers = $('.CounterDigits').height();
-		Counter++;
+		var WidthOfIndividualNumbers = $('.CounterDigits').width();
 		var iDiv = document.createElement('div');
+		Counter++;
 		iDiv.innerHTML = Counter;
 		iDiv.className = 'CounterDigits';
 		document.getElementById('NumberSeriesContainer').appendChild(iDiv);
 
-		if (((Counter % MinimumColumnHeightUserInput == 0) & (Counter <= (MinimumColumnHeightUserInput * MaximumNumberOfColumnsUserInput))) | (Counter < (MinimumColumnHeightUserInput))) {
+		if (Counter == (MinimumColumnHeightUserInput * 2)) {
 			$('div#NumberSeriesContainer').css({
-				height : (MinimumColumnHeightUserInput * HeightOfIndividualNumbers) + 'px'
+				height : (MinimumColumnHeightUserInput * HeightOfIndividualNumbers) + 'px',
+				width : ($('div#NumberSeriesContainer').width() + WidthOfIndividualNumbers) + 'px'
 			});
-		} else if ((Counter % (Math.floor(Counter / MinimumColumnHeightUserInput)) == 0) & (Counter < (MinimumColumnHeightUserInput * MaximumNumberOfColumnsUserInput))) {
+		} else if ((Counter > (MinimumColumnHeightUserInput * 2)) & (Counter < (MinimumColumnHeightUserInput * MaximumNumberOfColumnsUserInput))) {
 			$('div#NumberSeriesContainer').css({
-				height : ($('div#NumberSeriesContainer').height() + HeightOfIndividualNumbers) + 'px'
+				height : (Math.ceil(Counter / (Math.floor(Counter / MinimumColumnHeightUserInput))) * HeightOfIndividualNumbers) + 'px',
+				width : (Math.floor(Counter / MinimumColumnHeightUserInput) * WidthOfIndividualNumbers) + 'px'
 			});
-		} else if (Counter > (MinimumColumnHeightUserInput * MaximumNumberOfColumnsUserInput)) {
+		} else if (Counter >= (MinimumColumnHeightUserInput * MaximumNumberOfColumnsUserInput)) {
 			$('div#NumberSeriesContainer').css({
-				height : ((MinimumColumnHeightUserInput * HeightOfIndividualNumbers) + ((Math.floor((Counter - (MinimumColumnHeightUserInput * MaximumNumberOfColumnsUserInput)) / MaximumNumberOfColumnsUserInput) + 1) * HeightOfIndividualNumbers)) + 'px'
+				/*height : ((MinimumColumnHeightUserInput * HeightOfIndividualNumbers) + ((Math.floor((Counter - (MinimumColumnHeightUserInput * MaximumNumberOfColumnsUserInput)) / MaximumNumberOfColumnsUserInput) + 1) * HeightOfIndividualNumbers)) + 'px',*/
+				height : (Math.ceil(Counter / MaximumNumberOfColumnsUserInput) * HeightOfIndividualNumbers) + 'px',
+				width : (MaximumNumberOfColumnsUserInput * WidthOfIndividualNumbers) + 'px'
 			});
 		}
+
+		RelabelNumberSeriesElements()
 	}
 }
 
-window.setInterval("IncreaseItemsList()", 300); 
+function RelabelNumberSeriesElements() {
+	var div = document.getElementById('NumberSeriesContainer');
+	var divs = div.getElementsByTagName('div');
+	var MinimumColumnHeightUserInput = $('input#MinimumColumnHeight').val();
+	var MaximumNumberOfColumnsUserInput = $('input#MaximumNumberOfColumns').val();
+	var IndividualNumberContainerWidth = $('.CounterDigits').width();
+	var IndividualNumberContainerHeight = $('.CounterDigits').height();
+	var HeightOfEntireNumberContainer = $('div#NumberSeriesContainer').height();
+	var NumberSeriesElement = 0
+	var CompleteRectangleShapeLayoutIsPresent = false;
+	var Column, Row, ScaledSizeOfColumns, ElementsInLastRow;
+	for (var i = 0; i < divs.length; i += 1) {
+		Column = divs[i].offsetLeft / IndividualNumberContainerWidth
+		Row = divs[i].offsetTop / IndividualNumberContainerHeight
+		ScaledSizeOfColumns = HeightOfEntireNumberContainer / IndividualNumberContainerHeight
+		if (divs.length < (MinimumColumnHeightUserInput * MaximumNumberOfColumnsUserInput)) {
+			ElementsInLastRow = divs.length % (Math.floor(divs.length / MinimumColumnHeightUserInput))
+			CompleteRectangleShapeLayoutIsPresent = (divs.length % MinimumColumnHeightUserInput == 0 | divs.length % MaximumNumberOfColumnsUserInput == 0)
+		} else {
+			ElementsInLastRow = divs.length % MaximumNumberOfColumnsUserInput
+			CompleteRectangleShapeLayoutIsPresent = divs.length % MaximumNumberOfColumnsUserInput == 0
+		}
+
+		NumberSeriesElement = (Column * ScaledSizeOfColumns) + (Row + 1)
+		if ((Column > ElementsInLastRow) & !(CompleteRectangleShapeLayoutIsPresent) & !(ElementsInLastRow == 0)) {
+			NumberSeriesElement = NumberSeriesElement - (Column - ElementsInLastRow);
+		}
+		divs[i].innerHTML = NumberSeriesElement;
+	}
+}
+
+window.setInterval("IncreaseItemsList()", 300);
